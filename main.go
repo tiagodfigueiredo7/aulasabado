4e5021153140f4febaa274fc484bb43cd04b9460
tiagodfigueired7 <tiagod.figueiredo@gmail.com>
2022-09-03 13:54:15 -0300

    aulasabado

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+ 
+import (
+    "net/http"
+    "fmt"
+)
+ 
+func main() {
+ 
+    fmt.Printf("Começando!")
+     
+ 
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("<h1>Laboratorio de Sabado  </h1>"))
+ 
+    })
+ 
+    fmt.Printf("Inciando!")
+ 
+    http.ListenAndServe(":8080", nil)
+ 
+    fmt.Printf("Fim!")
+}
+ 
+
